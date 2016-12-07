25b2b29cbb7ec7c69ded985c397e45249cb39ad9
gwenf <gwenfaraday@gmail.com>
2016-12-07 11:13:03 -0500

    some code clean up

--- server.js
+++ server.js
@@ -13,13 +13,9 @@ app.use(bodyParser.json());
 
 app.get('/:time', function(req,res){
   var date = req.params.time;
-  // var unix = moment(date, "MMMM D, YYYY").format("X");
   var unix = null;
-  // var nat = moment.unix(unix).format("MMMM D, YYYY");
   var nat = null;
-  // console.log(parseInt(date))
 
-  // var isValid = moment(date, "MMMM D, YYYY").isValid();
   if (moment(date, "MMMM D, YYYY").isValid()){
     if (isNaN(parseInt(date))){
       unix = moment(date, "MMMM D, YYYY").format("X");
@@ -27,31 +23,13 @@ app.get('/:time', function(req,res){
       unix = date;
     }
     nat = moment.unix(unix).format("MMMM D, YYYY");
-    console.log('nat', nat)
-    // console.log('date valid', nat, unix, date);
 
     res.send({"unix":unix, "natural": nat});
-  // } else if (moment.unix(unix).isValid()){
-    // console.log('unix date');
-    // res.send({"unix":unix, "natural": nat});
+
   } else {
     console.log('date not valid');
     res.send({"unix": unix, "natural": nat});
   }
-
-  // console.log(date >= 0);
-  //
-  // console.log(unix);
-  // console.log(nat);
-  //
-  // if (unix){
-  //   res.send({"unix":unix, "natural": nat});
-  // } else {
-  //   res.send({"unix": null, "natural": null});
-  // }
-  //if req.params is a unix time stamp, return json with dates
-  //else return json with null
-  // res.send(req.params);
 });
 
 app.get('*', function(req,res){
